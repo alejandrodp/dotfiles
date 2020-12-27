1264e458f3bc4b3af06af97e33c675934d6a25b1
Alejandro DP <diaza913@gmail.com>
2020-12-26 23:10:34 -0600

    major update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,16 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
 # Terminate already running bar instances
 killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 # Launch bar
 echo "---" | tee -a /tmp/polybar.log
 
-#for m in $(polybar --list-monitors | cut -d":" -f1); do
-#    MONITOR=$m polybar example >>/tmp/polybar.log 2>&1 & disown
-#done
+for m in $(polybar --list-monitors | cut -d":" -f1) do
+    if [[ $m == 'eDP1' ]] then
+	MONITOR=$m polybar main >>/tmp/polybar.log 2>&1 & disown
+    elif [[ $m == 'HDMI1' ]] then
+	MONITOR=$m polybar external-monitor >>/tmp/polybar.log 2>&1 & disown
+    fi
+done
 
-MONITOR=eDP1 polybar example >>/tmp/polybar.log 2>&1 & disown
-MONITOR=HDMI1 polybar example2 >>/tmp/polybar.log 2>&1 & disown
+#MONITOR=eDP1 polybar main >>/tmp/polybar.log 2>&1 & disown
+#MONITOR=HDMI1 polybar external-monitor >>/tmp/polybar.log 2>&1 & disown
